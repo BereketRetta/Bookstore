e0ff1adedfb2e78815cfa9df5deae2f4521c6ab6
Beki <bereketadego@gmail.com>
2022-03-08 17:22:55 +0300

    add get books api

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/cofigureStore';
 import App from './App';
+import { getBooks } from './redux/books/book';
 
+store.dispatch(getBooks);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
